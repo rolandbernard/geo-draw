51e74a9eea64a5937798c35e216d58381081a225
Roland Bernard <rolli.bernard@gmail.com>
2020-10-29 17:52:31 +0100

    Added a router

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 
-console.log("Hello world!");
+import {Router} from "./router";
+
+window.addEventListener('load', () => {
+    const router = new Router();
+    router.add('#/(edit(/.*)?)?', () => {
+        console.log("edit");
+    });
+    router.add('#/view(/.*)?', () => {
+        console.log("view");
+    });
+    router.default(() => {
+        console.log("404");
+    });
+    router.execute();
+});
---
+++ src/router.js
@@ -0,0 +1,50 @@
+
+export class Router {
+    
+    constructor(){
+        this.routes = [];
+        this.default_routes = [];
+    }
+    
+    add(uri, callback) {
+        if(typeof uri === "string" && typeof callback === "function") {
+            const route = {
+                uri,
+                callback,
+            }
+            this.routes.push(route);
+        } else {
+            console.error("Failed to register route.", uri, callback);
+        }
+    }
+    
+    default(callback) {
+        if(typeof callback === "function") {
+            this.default_routes.push(callback);
+        } else {
+            console.error("Failed to register default route.", callback);
+        }
+    }
+
+    execute() {
+        let run_default = true;
+        this.routes.forEach(route => {
+            const regEx = new RegExp(`^${route.uri}$`);
+            let path = "";
+            if (route.uri[0] === "#") {
+                path = window.location.hash || "#/";
+            } else {
+                path = window.location.pathname || "/";
+            }            
+            if(path.match(regEx)){
+                let req = { path }
+                route.callback(req);
+                run_default = false;
+            }
+        });
+        if(run_default) {
+            this.default_routes.forEach(callback => callback());
+        }
+    }
+
+}
