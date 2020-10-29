a19b1537eee33caeec8fbf8ae480a9748f307d02
Roland Bernard <rolli.bernard@gmail.com>
2020-10-29 10:49:15 +0100

    Initial working state

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const webcomponentsjs = './node_modules/@webcomponents/webcomponentsjs';
+
+const polyfills = [
+  {
+    from: path.resolve(`${webcomponentsjs}/webcomponents-*.{js,map}`),
+    to: 'poly',
+    flatten: true
+  },
+  {
+    from: path.resolve(`${webcomponentsjs}/bundles/*.{js,map}`),
+    to: 'poly/bundles',
+    flatten: true
+  },
+  {
+    from: path.resolve(`${webcomponentsjs}/custom-elements-es5-adapter.js`),
+    to: 'poly',
+    flatten: true
+  },
+];
+
+const assets = [
+  {
+    from: 'static/',
+    to: 'static/'
+  },
+];
+
+const plugins = [
+  new CleanWebpackPlugin(['dist']),
+  new webpack.ProgressPlugin(),
+  new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: './src/index.html',
+    minify: {
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true
+    }
+  }),
+  new CopyWebpackPlugin([...polyfills, ...assets], {
+    ignore: ['.DS_Store']
+  }),
+  new MiniCssExtractPlugin(),
+];
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: '[name].[chunkhash:8].js',
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          plugins: ['@babel/plugin-syntax-dynamic-import'],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                useBuiltIns: 'usage',
+                targets: '>1%, not dead, ie 11'
+              }
+            ]
+          ]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  plugins: plugins,
+};
